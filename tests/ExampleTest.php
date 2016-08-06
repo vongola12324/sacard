3ef7f59c994b1e17cab2053f1f2c58a01818cf1d
Vongola <vongola12324@coder.tw>
2016-08-06 15:46:21 +0800

    Fix example test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -11,6 +11,6 @@ class ExampleTest extends TestCase
     public function testBasicExample()
     {
         $this->visit('/')
-             ->see('Laravel 5');
+             ->see('逢甲大學學生會特約商店');
     }
 }
